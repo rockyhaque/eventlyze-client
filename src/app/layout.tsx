8a814d1432bf2b31157193bee2841f07c5194775
Md Sakib al Hasan <mdsakibalhasanprogrammer1@gmail.com>
2025-04-30 14:01:26 +0600

    fix: image issue resolve

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,6 +27,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
          cz-shortcut-listen="true"
+          extension-installed="true"
       >
         {children}
       </body>
