54a00e603f20fcd9cc5a76b832a40ec9e7b3fae0
Md Sakib al Hasan <mdsakibalhasanprogrammer1@gmail.com>
2025-04-30 11:27:59 +0600

    wrip: add only ui designe

--- src/app/(WithCommonLayout)/login/page.tsx
+++ src/app/(WithCommonLayout)/login/page.tsx
@@ -1,9 +1,10 @@
+import LoginForm from "@/components/modules/auth/login/LoginForm";
 
 
 const LoginPage = () => {
   return (
-    <div>
-      LoginPage
+    <div className="min-h-screen flex items-center justify-center bg-gradient-to-b from-[#12081F] to-purple-900 p-4">
+      <LoginForm/>
     </div>
   );
 };
