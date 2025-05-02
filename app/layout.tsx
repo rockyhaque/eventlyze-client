640416fce872d39651d580831d37543576c94d64
Rashed <tools.rashed@gmail.com>
2025-05-02 23:42:35 +0600

    fix: login toast issue fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import { Mona_Sans as FontSans } from "next/font/google"
 import "./globals.css"
 import { cn } from "@/lib/utils"
 import { ThemeProvider } from "@/components/theme-provider"
+import { Toaster } from "sonner"
 
 
 const fontSans = FontSans({
@@ -28,10 +29,9 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning>
       <body className={cn("min-h-screen bg-background font-sans antialiased", fontSans.variable)}>
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
+        <Toaster position="top-left" richColors/>
           <div className="relative flex min-h-screen flex-col">
-  
             <main className="flex-1 min-h-screen">{children}</main>
-   
           </div>
         </ThemeProvider>
       </body>
