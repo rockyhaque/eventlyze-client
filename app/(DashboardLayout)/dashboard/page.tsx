fb3aee3614cf2d6f3b998e7be0baa5353ae0ff25
Rashed <rashed@notionhive.com>
2025-05-06 21:43:25 +0600

    feat: added user details utils funtion

--- app/(DashboardLayout)/dashboard/page.tsx
+++ app/(DashboardLayout)/dashboard/page.tsx
@@ -2,8 +2,13 @@ import { DashboardHeader } from "@/components/dashboard-header"
 import { DashboardStats } from "@/components/dashboard-stats"
 import { RecentInvitations } from "@/components/recent-invitations"
 import { RecentSubscriber } from "@/components/recent-subscriber"
+import { getActiveUser } from "@/hooks/getActiveUser"
+import { getStats } from "@/services/AdminServices"
 
-export default function DashboardPage() {
+export default async function DashboardPage() {
+  const {role} = await getActiveUser()
+  const stats = await getStats()
+
   return (
     <div className="space-y-4">
       <DashboardHeader />
---
+++ services/AdminServices/index.ts
@@ -0,0 +1,17 @@
+"use server"
+
+import app_axios from "@/lib/axios";
+
+
+export const getStats = async () => {
+    try {
+      const res = await app_axios.get(`/admin/stats`);
+      return res.data
+    } catch (error: any) {
+      console.log("error while getting single", error);
+      const message =
+        error?.response?.data?.message ||
+        "Something went wrong while getting stats!";
+      return new Error(message);
+    }
+  };
