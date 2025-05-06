b97fd182ef13dc0ab42c712db93ce39a256c4b3c
Rashed <rashed@notionhive.com>
2025-05-07 00:15:03 +0600

    fix: added chart for events count

--- components/dashboard-category-chart.tsx
+++ components/dashboard-category-chart.tsx
@@ -10,22 +10,18 @@ import {
 } from "@/components/ui/chart"
 
 const chartData = [
-  { month: "January", paid: 186, free: 80 },
-  { month: "February", paid: 305, free: 200 },
-  { month: "March", paid: 237, free: 120 },
-  { month: "April", paid: 73, free: 190 },
-  { month: "May", paid: 209, free: 130 },
-  { month: "June", paid: 214, free: 140 },
+  { month: "January", event: 186 },
+  { month: "February", event: 305},
+  { month: "March", event: 237},
+  { month: "April", event: 73},
+  { month: "May", event: 209},
+  { month: "June", event: 214},
 ]
 
 const chartConfig = {
-  paid: {
-    label: "Paid Event",
+  event: {
+    label: "Events Created",
     color: "#2563eb",
-  },
-  free: {
-    label: "Free Event",
-    color: "#60a5fa",
   },
 } satisfies ChartConfig
 
@@ -41,9 +37,8 @@ export function DashboardCategoryChart() {
           axisLine={false}
           tickFormatter={(value) => value.slice(0, 3)}
         />
-        <ChartTooltip content={<ChartTooltipContent />} />
-        <Bar dataKey="paid" fill="var(--color-paid)" radius={4} />
-        <Bar dataKey="free" fill="var(--color-free)" radius={4} />
+        <ChartTooltip content={<ChartTooltipContent className="w-[200px]" />} />
+        <Bar dataKey="event" fill="var(--color-event)" radius={4} />
       </BarChart>
     </ChartContainer>
   )
