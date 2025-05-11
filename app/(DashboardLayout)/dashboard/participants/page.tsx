3e50adc4b2508aa60e7659832dd1faac8bfaf902
FARUK2059 <omarfaruk34552@gmail.com>
2025-05-12 00:35:04 +0600

    fix: participant implement done

--- app/(DashboardLayout)/dashboard/participants/page.tsx
+++ app/(DashboardLayout)/dashboard/participants/page.tsx
@@ -1,14 +1,18 @@
 
 
 import ParticipantClientComponents from "@/components/modules/Participants/participantClientComponents";
+import { getAllParticipants } from "@/services/Participant";
 
 
 
-const ParticipantsServerComponents = () => {
+const ParticipantsServerComponents = async () => {
+    const allParticipants = await getAllParticipants();
+    const participants = allParticipants?.data
+
 
     return (
         <div>
-           <ParticipantClientComponents />
+            <ParticipantClientComponents participant={participants} />
         </div>
     );
 };
