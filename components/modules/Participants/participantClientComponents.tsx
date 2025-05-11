a176e4b85e3c280878746c8675d1ec9b85ff1df8
FARUK2059 <omarfaruk34552@gmail.com>
2025-05-11 22:05:00 +0600

    feat: participants components route done

---
+++ components/modules/Participants/participantClientComponents.tsx
@@ -0,0 +1,15 @@
+
+
+
+
+
+const ParticipantClientComponents = () => {
+
+    return (
+        <div>
+            <div> The Component is Start ParticipantClientComponents </div>
+        </div>
+    );
+};
+
+export default ParticipantClientComponents;
